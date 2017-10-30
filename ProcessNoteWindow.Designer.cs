cebb7a097e87bf432ec7c0551da0f8318f2ef10c
gabee1987 <gabee1987@gmail.com>
2017-10-30 16:59:16 +0100

    window forms window created, process name, id and memory usage displayed

---
+++ ProcessNoteWindow.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace process_note
+{
+    partial class ProcessNoteWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.processes = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.processGrid = new System.Windows.Forms.DataGridView();
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.processGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // processes
+            // 
+            this.processes.AutoSize = true;
+            this.processes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.processes.Location = new System.Drawing.Point(46, 32);
+            this.processes.Name = "processes";
+            this.processes.Size = new System.Drawing.Size(110, 25);
+            this.processes.TabIndex = 0;
+            this.processes.Text = "Processes:";
+            this.processes.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(635, 701);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get processes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // processGrid
+            // 
+            this.processGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.processGrid.Location = new System.Drawing.Point(51, 76);
+            this.processGrid.Name = "processGrid";
+            this.processGrid.RowTemplate.Height = 24;
+            this.processGrid.Size = new System.Drawing.Size(704, 607);
+            this.processGrid.TabIndex = 2;
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(process_note.Program);
+            // 
+            // ProcessNoteWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.processGrid);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.processes);
+            this.Name = "ProcessNoteWindow";
+            this.Text = "                  ";
+            ((System.ComponentModel.ISupportInitialize)(this.processGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label processes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.DataGridView processGrid;
+    }
+}
---
+++ ProcessNoteWindow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace process_note
+{
+    public partial class ProcessNoteWindow : Form
+    {
+        public ProcessNoteWindow()
+        {
+            InitializeComponent();
+            loadProcesses();   
+        }
+
+        private void loadProcesses()
+        {
+            Process[] localAll = Process.GetProcesses();
+
+            processGrid.ColumnCount = 3;
+            processGrid.Columns[0].Name = "Process ID";
+            processGrid.Columns[1].Name = "Process Name";
+            processGrid.Columns[2].Name = "Memory usage";
+
+
+            foreach (var process in localAll)
+            {
+                string[] rowName = { process.Id.ToString(),  process.ProcessName, (process.WorkingSet64 / 1048576).ToString() + " MB" };
+                processGrid.Rows.Add(rowName);
+            }
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
